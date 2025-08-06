001ad443538faa1fad6e3684239dbcf1c92137b7
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-08-06 20:37:58 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,11 +3,13 @@ from frontend.Home import show_home
 from frontend.Results import show_results
 from frontend.Feedback import show_feedback
 from frontend.History import show_history
+from frontend.LanguageSelector import language_selector
 
 st.set_page_config(page_title="Mediscope-AI", layout="wide")
 
 PAGES = {
     "Home": show_home,
+    "Language": language_selector,
     "Results": show_results,
     "Feedback": show_feedback,
     "History": show_history
