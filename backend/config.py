303c816e5a84bcdc21809e122859e60ff4998280
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-08-06 20:07:53 +0530

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+API_SETTINGS = {
+    "host": "127.0.0.1",
+    "port": 8000
+}
+
+MODEL_NAMES = {
+    "summarizer": "facebook/bart-large-cnn",
+    "qa_model": "deepset/roberta-base-squad2"
+}
+
+UPLOAD_FOLDER = "uploads"
