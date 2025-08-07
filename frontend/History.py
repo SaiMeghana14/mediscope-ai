e4f4743a561d5ba8bba4c72014c7d0ecb13b23a0
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-08-07 12:48:41 +0530

    Update History.py

--- frontend/History.py
+++ frontend/History.py
@@ -1,6 +1,8 @@
 import streamlit as st
-
+from database import fetch_history
 def show_history():
-    st.title("📜 Previous Reports")
+     st.title("📜 Previous Reports")
     st.info("This section can be connected to your database or storage to fetch patient history.")
-    st.write("🔒 Coming Soon: Secure patient history and AI diagnosis retrieval.")
+    history = fetch_history()
+    for t, d, c in history:
+        st.markdown(f"**🕒 {t}** — _{d}_ — Confidence: {c*100:.1f}%")
