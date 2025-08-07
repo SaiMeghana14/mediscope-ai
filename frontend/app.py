29c78f518510a19cc8edc04fcc49ec3bee9199a6
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-08-07 12:25:12 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,11 +5,11 @@ from frontend.Feedback import show_feedback
 from frontend.History import show_history
 from frontend.LanguageSelector import language_selector
 
-with open("frontend/styles.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
 st.set_page_config(page_title="Mediscope-AI", layout="wide")
 
+with open("frontend/styles.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    
 PAGES = {
     "Home": show_home,
     "Language": language_selector,
