62cf1363efa27941a49ac4dbcb09971648811731
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-08-07 12:49:24 +0530

    Update LanguageSelector.py

--- frontend/LanguageSelector.py
+++ frontend/LanguageSelector.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from deep_translator import GoogleTranslator
 
 def select_language():
     return st.selectbox("Select Language", ["en", "fr"])
+
+def translate_text(text, target_lang='hi'):
+    return GoogleTranslator(source='auto', target=target_lang).translate(text)
